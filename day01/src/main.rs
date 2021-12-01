8c8429d1cf8f2db488d3cfa057a8fce0ea4646ec
Douglas Parsons <dglsparsons@gmail.com>
2021-12-01 08:05:22 +0000

    Day 01

---
+++ day01/src/main.rs
@@ -0,0 +1,35 @@
+use std::convert::TryFrom;
+use std::fs;
+
+fn main() {
+    let file = fs::read_to_string("./input.txt").expect("unable to read input file");
+
+    let measurements: Vec<usize> = file
+        .lines()
+        .map(|s| s.parse::<usize>().expect("couldn't parse number"))
+        .collect();
+
+    part_one(&measurements);
+
+    part_two(&measurements);
+}
+
+fn part_one(measurements: &[usize]) {
+    let count = measurements
+        .windows(2)
+        .flat_map(<&[usize; 2]>::try_from)
+        .filter(|[a, b]| b > a)
+        .count();
+
+    println!("part one - count: {}", count);
+}
+
+fn part_two(measurements: &[usize]) {
+    let count = measurements
+        .windows(4)
+        .flat_map(<&[usize; 4]>::try_from)
+        .filter(|[a, b, c, d]| b + c + d > a + b + c)
+        .count();
+
+    println!("part two - count: {}", count);
+}
